ab1d1b495e9a81f02303e99539f0b7e1f0f220eb
ShiyumeMeguri <102138304+ShiyumeMeguri@users.noreply.github.com>
2026-05-26 09:18:39 +0900

    delete redundant BundledAssetsExportMode hook

--- Source/Ruri.RipperHook/Core/GameType.cs
+++ Source/Ruri.RipperHook/Core/GameType.cs
@@ -15,7 +15,6 @@ public enum GameType
     AR_ShaderDecompiler,
     AR_SkipStreamingAssetsCopy,
     AR_StaticMeshSeparation,
-    AR_BundledAssetsExportMode,
     AR_SkipProcessingAnimation,
     AR_ExportDirectly,
     AR_AnimationExport,
